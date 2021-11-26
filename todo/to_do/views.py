03e380e88976b7396632492249526e5608527f70
georgek2 <george22k44@gmail.com>
2021-11-26 18:34:35 +0300

    Study topics tracker

---
+++ todo/to_do/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from .models import *
+
+from .forms import *
+# Create your views here.
+
+
+def homePage(request):
+
+    tasks = Task.objects.all()
+    form = TaskForm()
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+        return redirect('/')
+
+    context = {
+        'tasks': tasks,
+        'form': form
+    }
+
+    return render(request, 'to_do/index.html', context)
+
+
+def update(request, pk):
+
+    task = Task.objects.get(id=pk)
+    form = TaskForm(instance=task)
+
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            form.save()
+
+        return redirect('/') 
+
+    context = {
+        'task': task,
+        'form': form
+    }
+
+    return render(request, 'to_do/task.html', context)
+
+
+
+
+
+
